c95318d52d1f3225fe8f87365c1e667b07840dd3
otofu-square <yukita@feedforce.jp>
2017-08-30 23:21:57 +0900

    :+1: Introduce tiny react app by fuse-box

--- fuse.ts
+++ fuse.ts
@@ -1,10 +1,17 @@
-import { FuseBox } from "fuse-box";
+import { FuseBox, WebIndexPlugin } from "fuse-box";
 
 const fuse = FuseBox.init({
   homeDir: "src",
   output: "build/$name.js",
+  plugins: [
+    WebIndexPlugin({
+      path: "./",
+      title: "Apollo sample app",
+      template: "src/index.html",
+    }),
+  ],
 });
 
-fuse.bundle("bundle").instructions(`>index.ts`);
+fuse.bundle("bundle").instructions(`>index.tsx`);
 
 fuse.run();
