e8bb3a7c2acbed32e3c75dcfe3b78cdf43e62d68
otofu-square <yukita@feedforce.jp>
2017-08-31 23:56:51 +0900

    :+1: Introduce react-apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,25 @@
 import * as React from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface,
+} from "react-apollo";
 import * as ReactDOM from "react-dom";
 
 import App from "./App";
 
-const Root = () => <App />;
+const networkInterface = createNetworkInterface({
+  uri: process.env.GRAPHCOOL_ENDPOINT,
+});
+
+const client = new ApolloClient({
+  networkInterface,
+});
+
+const Root = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
 
 ReactDOM.render(<Root />, document.getElementById("root"));
