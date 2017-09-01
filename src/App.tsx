c86968b5bfbdc9f537cc1b8e381571b9d4437ed6
otofu-square <yukita@feedforce.jp>
2017-09-01 14:09:14 +0900

    :+1: Add LinkList component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import * as React from "react";
 
-const App = () => <h1>Hello World!!</h1>;
+import LinkList from "./components/LinkList";
+
+const defaultProps = [
+  {
+    id: "1",
+    description: "The Coolest GraphQL Backend 😎",
+    url: "https://www.graph.cool",
+  },
+  {
+    id: "2",
+    description: "The Best GraphQL Client",
+    url: "http://dev.apollodata.com/",
+  },
+];
+
+const App = () => <LinkList links={defaultProps} />;
 
 export default App;
--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -7,22 +7,7 @@ interface IProps {
   links: ILink[];
 }
 
-const defaultProps = {
-  links: [
-    {
-      id: "1",
-      description: "The Coolest GraphQL Backend 😎",
-      url: "https://www.graph.cool",
-    },
-    {
-      id: "2",
-      description: "The Best GraphQL Client",
-      url: "http://dev.apollodata.com/",
-    },
-  ],
-};
-
-const LinkList = ({ links }: IProps = defaultProps) => (
+const LinkList = ({ links }: IProps) => (
   <div>
     {links.map(link => (
       <Link key={link.id} url={link.url} description={link.description} />
