acfb5d40c95173534974739325ee0f1fa918785c
otofu-square <yukita@feedforce.jp>
2017-09-09 22:57:53 +0900

    :+1: Add CreateLink component

---
+++ src/components/CreateLink.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+class CreateLink extends React.Component {
+  public state = {
+    description: "",
+    url: "",
+  };
+
+  public render() {
+    return (
+      <div>
+        <div className="flex flex-column mt3">
+          <input
+            className="mb2"
+            value={this.state.description}
+            onChange={e => this.setState({ description: e.target.value })}
+            type="text"
+            placeholder="A description for the link"
+          />
+          <input
+            className="mb2"
+            value={this.state.url}
+            onChange={e => this.setState({ url: e.target.value })}
+            type="text"
+            placeholder="The URL for the link"
+          />
+        </div>
+        <button onClick={() => this.createLink()}>Submit</button>
+      </div>
+    );
+  }
+
+  public createLink = async () => ({});
+}
+
+export default CreateLink;
