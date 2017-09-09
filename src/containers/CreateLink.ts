b69b000c3232092c066405edf9d01351da174f65
otofu-square <yukita@feedforce.jp>
2017-09-09 23:32:09 +0900

    :wrench: Fixed mutation query

--- src/containers/CreateLink.ts
+++ src/containers/CreateLink.ts
@@ -13,11 +13,8 @@ interface IProps {
 }
 
 export const CREATE_LINK_MUTATION = gql`
-  mutation CreateLinkMutation($description String!, $url: String!) {
-    createLink (
-      description: $description,
-      url: $url,
-    ) {
+  mutation CreateLinkMutation($description: String!, $url: String!) {
+    createLink(description: $description, url: $url) {
       id
       createdAt
       url
