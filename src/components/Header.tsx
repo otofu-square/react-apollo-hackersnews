c98f24af7a2847bb7b3498015afb5d731215c1cc
otofu-square <yukita@feedforce.jp>
2017-09-10 09:34:24 +0900

    :+1: Add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+
+const Header = () => (
+  <div className="flex pa1 justify-between nowrap orange">
+    <div className="flex flex-fixed black">
+      <div className="fw7 mr1">Hacker News</div>
+      <Link to="/" className="ml1 no-underline black">
+        new
+      </Link>
+      <div className="ml1">|</div>
+      <Link to="/create" className="ml1 no-underline black">
+        submit
+      </Link>
+    </div>
+  </div>
+);
+
+export default withRouter(Header);
